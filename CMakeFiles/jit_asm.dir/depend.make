89920f2f5963abeee29253acb7b5bfc62ea8a6ac
张涔 <21371365@buaa.edu.cn>
2024-07-25 11:17:36 +0800

    init

---
+++ CMakeFiles/jit_asm.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for jit_asm.
+# This may be replaced when dependencies are built.
---
+++ src/include/jitter.hpp
@@ -0,0 +1,37 @@
+#ifndef OMVLL_JITTER_H
+#define OMVLL_JITTER_H
+#include <string>
+#include <memory>
+
+#include <llvm/ADT/IntrusiveRefCntPtr.h>
+#include <llvm/ADT/StringRef.h>
+#include <llvm/Support/Error.h>
+
+namespace llvm {
+class LLVMContext;
+class Module;
+class MemoryBuffer;
+namespace orc {
+class LLJIT;
+}
+
+namespace object {
+class ObjectFile;
+}
+}
+
+namespace Kotoamatsukami {
+class Jitter {
+public:
+  Jitter(const std::string &Triple);
+
+  std::unique_ptr<llvm::orc::LLJIT> compile(llvm::Module& M);
+
+  std::unique_ptr<llvm::MemoryBuffer> jitAsm(const std::string& Asm, size_t Size);
+
+private:
+  std::string Triple_;
+  std::unique_ptr<llvm::LLVMContext> Ctx_;
+};
+}
+#endif
