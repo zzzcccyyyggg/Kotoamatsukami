18f283df65f10a4dbb14c6f730994a14f20ed901
ZCVM <1539412714@qqq.com>
2024-12-19 23:54:06 +0800

    remove the scripts from cache

--- scripts/compile.sh
+++
@@ -1,5 +0,0 @@
-# 设置 clang、opt 和插件路径
-CLANG=/home/zzzccc/llvm-17/llvm-project/build/bin/clang-17
-OPT=/home/zzzccc/llvm-17/llvm-project/build/bin/opt
-Kotoamatsukami_so=/home/zzzccc/cxzz/Kotoamatsukami/build/Kotoamatsukami.so
-$CLANG -fpass-plugin=$Kotoamatsukami_so "$@"
